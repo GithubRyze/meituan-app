b01a73124185ce4d6291f0bb6cf149c7140f723a
ryze <m18588448605@163.com>
2018-04-26 15:03:53 +0800

    complete seach address:

--- src/component/home/AddressSeacher.js
+++ src/component/home/AddressSeacher.js
@@ -3,13 +3,19 @@ class AddressSeacher extends React.Component{
     render(){
         return(
             <div className="top-map">
-                <a className="city"> 
-                    <span>深圳</span>
-                    <b className="trangle-down"></b>
-                    <span className="dvd-line"></span>
-                </a>
-                <input type="text" autoComplete="off" placeholder="输入地址搜索周边美食" className="seacher-input"/>
-                <a >搜索</a>               
+                <div className="top">
+                    <div className="address m-shadow clearfix">
+                        <a className="city fl"> 
+                            <span>深圳</span>
+                            <b className="trangle-down  "></b>
+                            <span className="dvd-line"></span>
+                        </a>
+                    </div>
+                    <div className="fl">
+                        <input type="text" autoComplete="off" placeholder="输入地址搜索周边美食" className="seacher-input"/>
+                        <a className="div-search-input-a">搜索</a>    
+                    </div> 
+                </div>          
             </div>
         );
     }
